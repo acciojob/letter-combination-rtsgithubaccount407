e0c2e7c92596cd56833a84937a39815f35074c01
Rohit Sharma <123295893+rtsgithubaccount407@users.noreply.github.com>
2023-06-16 16:35:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
 function letterCombinations(input) {
   //Complete the function
 
-	let table ={
-					"2":"abc";
-					"3":"def";
-					"4":"ghi";
-					"5":"jkl";
-					"6":"mno";
-					"7":"pqrs";
-					"8":"tuv";
-					"9":"wxyz";
-	}
-let solution =[];
-
-function solve(currentIndex, calculatedString){
-	if(input.length<=currentindex){
-		solution.push(calculatedString);
-		return;
-	}
-	let currentChar = input[currentIndex]
-	for(let i=0; i<table[currentChar].length; i++){
-		solve(currentIndex+1, calculatedString + table[currentChar[i])
-	}
-	
+	let table = {
+    "2" : "abc",
+    "3" : "def",
+    "4" : "ghi",
+    "5" : "jkl",
+    "6" : "mno",
+    "7" : "pqrs",
+    "8" : "tuv",
+    "9" : "xyz"
 }
-solve(0,"")
-retrun solution;
+
+let solutions = [] ;
+
+function solve(currentIndex, calculatedString) {
+    if(input.length <= currentIndex){
+        solutions.push(calculatedString);
+        return;
+    }
+    // currentIndex = 2, calculatedString = "dm"
+    let currentChar = input[currentIndex] // "3"
+    // table["3"] = "def"
+    for(let i = 0 ; i < table[currentChar].length; i++) {
+        // "def" 
+        solve(currentIndex + 1, calculatedString + table[currentChar][i] )
+        // solve(0 + 1, "" + "d")
+    }
+}
+
+solve(0, "")
+
+console.log(solutions)
+
 module.exports = letterCombinations;
