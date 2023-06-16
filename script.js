1175d07c063088f7f6f83d9e137dfdb9ffd745f2
Rohit Sharma <123295893+rtsgithubaccount407@users.noreply.github.com>
2023-06-16 16:23:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-function letterCombinations(input_digit) {
+function letterCombinations(input) {
   //Complete the function
-}
 
+	let table ={
+					"2":"abc";
+					"3":"def";
+					"4":"ghi";
+					"5":"jkl";
+					"6":"mno";
+					"7":"pqrs";
+					"8":"tuv";
+					"9":"wxyz";
+	}
+let solution =[];
+
+function solve(currentIndex, calculatedString){
+	if(input.length<=currentindex){
+		solution.push(calculatedString);
+		return;
+	}
+	let currentChar = input[currentIndex]
+	for(let i=0; i<table[currentChar].length; i++){
+		solve(currentIndex+1, calculatedString + table[currentChar[i])
+	}
+	
+}
+solve(0,"")
+retrun solution;
 module.exports = letterCombinations;
